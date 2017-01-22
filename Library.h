8de29517fbab2886b7ae8ed3e0e1135ed7ee9271
Orachigami <orachigami@mail.ru>
2017-01-23 01:15:18 +0300

    Update Library.h
    Added ActionQueue feature:
    *Structure for an Action Frame
    *ActionQueue array

--- Library.h
+++ Library.h
@@ -1,8 +1,8 @@
 #ifndef ArboLib
 #define ArboLib
 #include <GL/freeglut.h>
-unsigned char pixel[3] = { 255, 0, 0 };
-
+unsigned char pixel[3] = { 0, 255, 0 };
+unsigned char red[3] = { 255, 0, 0 };
 typedef struct _GameObject
 {
 	unsigned char busy;
@@ -11,12 +11,12 @@ typedef struct _GameObject
 	struct model* animation;
 	int x;
 	int y;
-	int lx, ty, rx;	//Крайние точки
-	signed char vector;
-	unsigned char air_state;
-	unsigned char weight;
+	int lx, ty, rx;	//Object Edge
+	signed char vector; //Eyesight vector
+	unsigned char air_state; //0 - ground, 1 - flies up, 2 - falls
+	unsigned char weight; //Falling offset
 } GameObject;
-
+//Animation's frame structure
 struct model
 {
 	unsigned char* frame;
@@ -25,7 +25,7 @@ struct model
 	int frames;
 	int name;
 	unsigned char loop;
-	unsigned char interrupt;
+	unsigned char interruptable;
 	int height;
 	int width;
 };
@@ -46,6 +46,8 @@ typedef struct _GameActor
 	int vitality;
 	int mysticism;
 	int blood_rust;
+	//Actor controller status
+	char stunned;
 }GameActor;
 typedef struct _Enemy
 {
@@ -59,28 +61,29 @@ typedef struct _Entity
 	short invulnerable;
 	short walkthrough;
 }Entity;
-
+//A union for universal work with the GameObjects
 union _Convertor
 {
 	struct _GameObject* toGameObject;
 	struct _GameActor* toGameActor;
-
 }Convertor;
 
 GameActor player;
-
+//An image with width(sizeX) and height(sizeY)
+//+Data
 typedef struct _AUX_RGBImageRec
 {
 	GLint sizeX, sizeY;
 	unsigned char *data;
 } AUX_RGBImageRec;
+//An array where all loaded images are located
 struct _mainArray
 {
 	AUX_RGBImageRec *main_arr;
 	int length;
 } main_array;
 #define mainArray main_array.main_arr
-
+//The structure that describes a certain area/zone
 struct response
 {
 	int id;
@@ -94,25 +97,59 @@ struct _OrderToClear
 	struct response* area;
 	int last_id;
 } OrderToClear;
+//The structure of one frame for the FrameOrder
 struct line
 {
+	//The time when the frame was set
 	long sent_time;
+	//How much time should the frame be shown
 	int msecs;
 	GameObject* object;
+	//The frame number
 	int frame;
 };
+//The FrameOrder
 struct line order[10];
+//A structure of one action for the ActionQueue
+struct row
+{
+	//The time when the action was set
+	long sent_time;
+	//Action func
+	void (*todo) (struct row*);
+	//Subject
+	GameObject* object;
+	//Milliseconds
+	int msecs;
+	//Additional variable 1
+	int help1;
+	//Additional variable 2
+	int help2;
+	//Action's stage
+	int step;
+};
+//The ActionQueue
+struct row* queue;
 enum Animations
 {
 	idle = 0,
 	run0 = 1,
 	run1 = 2,
-	jump = 3,
-	attack01 = 4
+	jump0 = 3,
+	jump1 = 4,
+	attack01 = 5
 
 };
+//Magic 2D path eye
 unsigned char** TechMap;
+//Image names
 char** StringPool;
+
 void moveObject(GameObject *object, int key);
 void playerController(int id);
+void playAnimation(GameObject* object, int animation);
+
+int resetAnimation(GameObject* object);
+int times;
+Entity grass;
 #endif // !ArboLib
