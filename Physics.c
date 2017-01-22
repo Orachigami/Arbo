e2443589d609fd5805c157e9c55b10f01bf7424c
Orachigami <orachigami@mail.ru>
2017-01-23 00:56:37 +0300

    Update Physics.c
    Added ActionQueue feature with Add/Remove actions and the "iterator" cycle for managing actions.
     + Alpha Jump Maths Logic version;

--- Physics.c
+++ Physics.c
@@ -2,10 +2,16 @@
 #define PHYSICS_C
 #include "Library.h"
 #include "Controller.c"
+#include "Animator.c"
+#include <math.h>
 //FALL
 //JUMP
+//Action queue length
+static int action_length = 0;
+
 //OVERSTEP
 
+//Movement logic/behaviour for overstepping blocks
 void overStep(GameObject* object, int adm)
 {
 	int x = 0, y = 0, current_x = object->x,
@@ -31,7 +37,7 @@ void overStep(GameObject* object, int adm)
 	object->x += x;
 	object->y += y;
 }
-
+//Tests the "roof" above the game object
 int isNearRoof(GameObject* object)
 {
 	int realHeight = mainArray[object->animation[object->current_animation].id].sizeY / object->animation[object->current_animation].frames,
@@ -45,7 +51,8 @@ int isNearRoof(GameObject* object)
 
 	return 0;
 }
-
+//Tests the ground under the game object
+//*Uses TechMap: 1 - wall/ground, 0 - air
 int isOnGround(GameObject* object, int doFall)
 {
 	int current_x = object->x, code = 1, y = 0;
@@ -59,7 +66,7 @@ int isOnGround(GameObject* object, int doFall)
 		current_x += object->rx;
 		if (TechMap[current_x][object->y - 1] == 0) object->air_state = 2;
 	}
-	if (object->air_state == 2 && TechMap[current_x][object->y] == 0) code = 0;
+	if (object->air_state == 2 && TechMap[current_x][object->y - 1] == 0) code = 0;
 	if (doFall)
 	{
 		for (; TechMap[object->x + object->lx + 1][object->y + y - 1] != 1 && TechMap[object->x + object->rx - 1][object->y + y - 1] != 1 && TechMap[object->x + object->animation[object->current_animation].height / 2][object->y + y - 1] != 1 && -y != object->weight;)
@@ -69,5 +76,88 @@ int isOnGround(GameObject* object, int doFall)
 		object->y += y;
 	}
 	return code;
+}
+//Maths jump logic
+void jumpProceed(struct row* subject)
+{
+	int step = subject->step;
+	union _Convertor obj;
+	int y = subject->object->y,
+		x = subject->object->x;
+	GameActor* actor;
+	GameObject* object;
+	obj.toGameObject = subject->object;
+	actor = obj.toGameActor;
+	object = obj.toGameObject;
+	object->x += actor->speed*object->vector;
+	object->y = -1* pow((object->x - actor->speed*sqrt(actor->jump)*object->vector - subject->help1)/actor->speed,2) + actor->jump + subject->help2;
+	//printf("X: %d; Y: %d;\n", object->x, object->y);
+	drawObject(object, x, y);
+	subject->step++;
+	if (isOnGround(object, 0))
+	{
+		actor->stunned = 0;
+		return;
+	}
+	addToQueue(subject);
+}
+//Initializates action row for the "Jump" action
+void Jump(union _Convertor obj)
+{
+	struct row* startJump = malloc(sizeof(struct row));
+	GameActor* actor = obj.toGameActor;
+	GameObject* object = obj.toGameObject;
+	actor->stunned++;
+	if (resetAnimation(obj.toGameObject))
+	{
+		actor->itself.busy = 0;
+		object->current_animation = jump0;
+	}
+	//playAnimation(object, jump0);
+	startJump->object = object;
+	startJump->todo = jumpProceed;
+	startJump->step = 0;
+	startJump->sent_time = GetTickCount();
+	startJump->msecs = 50;
+	startJump->help1 = object->x;
+	startJump->help2 = object->y;
+	addToQueue(startJump);
+}
+//Updates action's current stage time and adds action row into the Queue
+void addToQueue(struct row* subject)
+{
+	static int dozen = 1;
+	if (++action_length >= dozen * 10) realloc(queue, sizeof(struct row) * 10 * (++dozen));
+	subject->sent_time = GetTickCount();
+	queue[action_length - 1] = *subject;
+}
+//**Removes the action from the ActionQueue via it's id
+void removeFromQueue(int id)
+{
+	register int i = id;
+	union _Convertor obj;
+	obj.toGameObject = queue[id].object;
+	--action_length;
+	for (; i < action_length; i++)
+		queue[i] = queue[i + 1];
+}
+//Global cycle which looks for the action that should be updated
+//Uses Ticks for checking action's time
+//P.S. mb concatenate with animatior.c "iterate" or/and player controller
+void iterator(int timer_id)
+{
+	register int i = 0;
+	register long now = GetTickCount();
+	for (i; i < action_length; i++)
+		if (now - queue[i].sent_time >= queue[i].msecs)
+		{
+			queue[i].todo(&queue[i]);
+			removeFromQueue(i);
+		}
+	//i = GetTickCount() - now - 16;
+	//if (i <= 0) i = 0;
+	//else
+	//printf("---------\nACTION: %d\n---------", i);
+	glutTimerFunc(i, iterator, timer_id);
 }
 #endif // !PHYSICS_C
